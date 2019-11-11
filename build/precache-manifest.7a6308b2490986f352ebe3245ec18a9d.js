e4b2003d0a2055d82752871067deb0873834f406
Adarsh <adarsh.yadav@relesol.com>
2019-11-11 18:06:10 +0530

    ignore updated

---
+++ build/precache-manifest.7a6308b2490986f352ebe3245ec18a9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "74c01adda3ab1ded5c1b",
+    "url": "/static/js/main.74c01add.chunk.js"
+  },
+  {
+    "revision": "b3a89d6947bbf1f39e27",
+    "url": "/static/js/1.b3a89d69.chunk.js"
+  },
+  {
+    "revision": "74c01adda3ab1ded5c1b",
+    "url": "/static/css/main.3cb4dcbd.chunk.css"
+  },
+  {
+    "revision": "26c498b3ca78b20f39479854062b1e4c",
+    "url": "/index.html"
+  }
+];
